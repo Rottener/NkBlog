d59e9743cf7f9a47cde1f7d734630a4b81cbebb4
Rotten <Rotten@DESKTOP-GF4PAET>
2019-03-06 22:26:00 +0800

    1

---
+++ NkBolg.Common/Log/LoginLogHandler.cs
@@ -0,0 +1,48 @@
+﻿using NkBolg.Common.Auth;
+using NkBolg.Common.Net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NkBolg.Common.Log
+{
+    /// <summary>
+    /// 系统用户登录日志处理
+    /// </summary>
+    public class LoginLogHandler : LogHandler<LoginLog>
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="loginId">登录id</param>
+        public LoginLogHandler(string loginId) : base(LogMode.LoginLog)
+        {
+            AuthorizationUser currentUser = null;
+            var current = HttpContextHelper.Current;
+            if (current != null)
+            {
+                currentUser = AuthenticationHelper.Current();
+            }
+            if (currentUser == null)
+            {
+                currentUser = new AuthorizationUser()
+                {
+                    RealName = "匿名用户"
+                };
+            }
+            var request = current.Request;
+            LogInfo = new LoginLog
+            {
+                LoginId = loginId,
+                CreateAccountId = currentUser.AccountId,
+                ServerHost = HttpHelper.GetServerIp(),
+                ClientHost = HttpHelper.GetClientIp(),
+                UserAgent = HttpHelper.UserAgent(),
+                OsVersion = HttpHelper.GetOsVersion(),
+                LoginTime = DateTime.Now,
+                IpAddressName = HttpHelper.GetAddressByApi()
+            };
+
+        }
+    }
+}
